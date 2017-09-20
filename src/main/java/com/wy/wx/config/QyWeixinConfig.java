840171e87d49c4b8ee7757dd6257c9b55ada03a7
wjxiongw <wjxiongw@163.com>
2017-09-20 20:40:50 +0800

    htwx

--- src/main/java/com/wy/wx/config/QyWeixinConfig.java
+++ src/main/java/com/wy/wx/config/QyWeixinConfig.java
@@ -6,14 +6,15 @@ package com.wy.wx.config;
  */
 public class QyWeixinConfig {
 	//门店互动平台
-	public static final String CORPID = "wx19d8777d071af48a";
+	public static final String CORPID = "";
 	public static final String AGENTID = "0";
-	public static final String SECRET = "T0IkUMgjp33-sQ4zwPl08M9Yqw51-wSsKVlgju3Co3-J6YoLURBKUTFDZjbhBCa5";
+	public static final String SECRET = "";
 	//获取access_token
 	public static final String ACCESS_TOKEN_URL = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid="+CORPID+"&corpsecret="+SECRET;
 	//读取成员
 	public static final String READ_USRE_URL = "https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token=ACCESS_TOKEN&userid=USERID";
 	//发送消息
 	public static final String SEND_MSG_URL = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=ACCESS_TOKEN";
-	
+	//发送图片
+	public static final String SEND_PIC_URL = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=ACCESS_TOKEN";
 }
